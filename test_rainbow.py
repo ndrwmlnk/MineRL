177e1901c38393586fb383935589532eb2a15d61
Giacomo Leidi <goodoldpaul@autistici.org>
2020-01-23 17:24:56 +0100

    test_rainbow: Remove unused imports.
    * test_rainbow.py: Remove unused imports.

--- test_rainbow.py
+++ test_rainbow.py
@@ -1,65 +1,51 @@
 # Simple env test.
-import json
-import select
-import time
-import logging
-from logging import getLogger
-import os
-
-import gym
+# noinspection PyUnresolvedReferences
 import minerl
 
-import numpy as np
+import os
+import sys
+import time
 
 import chainer
-from chainer import cuda
-
 import chainerrl
-from chainerrl.wrappers import ContinuingTimeLimit, RandomizeAction
-from chainerrl.wrappers.atari_wrappers import FrameStack#, ScaledFloatFrame
+import gym
+import numpy as np
+from chainer import cuda
+from chainerrl.wrappers import ContinuingTimeLimit
+from chainerrl.wrappers.atari_wrappers import FrameStack
 from chainerrl.wrappers.atari_wrappers import ScaledFloatFrame as SFF
 
-import sys
 sys.path.append(os.path.abspath(os.path.join(__file__, os.pardir)))
-from utility import utils
-from utility.q_functions import DuelingDQN, DistributionalDuelingDQN
-from utility.env_wrappers import (
-    SerialDiscreteActionWrapper, CombineActionWrapper, SerialDiscreteCombineActionWrapper,
-    ContinuingTimeLimitMonitor,
-    MoveAxisWrapper, FrameSkip, FrameStack, ObtainPoVWrapper, FullObservationSpaceWrapper)
+
+from utility.q_functions import DistributionalDuelingDQN
+from utility.env_wrappers import (SerialDiscreteActionWrapper, MoveAxisWrapper, FrameSkip, FrameStack, ObtainPoVWrapper)
 
 # All the evaluations will be evaluated on MineRLObtainDiamond-v0 environment
 # MINERL_GYM_ENV = os.getenv('MINERL_GYM_ENV', 'MineRLObtainDiamond-v0')
 MINERL_GYM_ENV = os.getenv('MINERL_GYM_ENV', 'MineRLTreechop-v0')
 MINERL_MAX_EVALUATION_EPISODES = int(os.getenv('MINERL_MAX_EVALUATION_EPISODES', 5))
 
+
 def wrap_env(env, test):
-        # wrap env: time limit...
-        if isinstance(env, gym.wrappers.TimeLimit):
-            env = env.env
-            max_episode_steps = env.spec.max_episode_steps
-            env = ContinuingTimeLimit(env, max_episode_steps=max_episode_steps)
-        # wrap env: observation...
-        # NOTE: wrapping order matters!
-        env = FrameSkip(env, skip=4)
-        env = ObtainPoVWrapper(env)
-        # convert hwc -> chw as Chainer requires.
-        env = MoveAxisWrapper(env, source=-1, destination=0)
-        # env = ScaledFloatFrame(env)
-        env = SFF(env)
-        env = FrameStack(env, 4, channel_order='chw')
-        # wrap env: action...
-        env = SerialDiscreteActionWrapper(
-            env,
-            always_keys=['attack'], reverse_keys=['forward'], exclude_keys=['back', 'left', 'right', 'sneak', 'sprint'], exclude_noop=False)
+    # wrap env: time limit...
+    if isinstance(env, gym.wrappers.TimeLimit):
+        env = env.env
+        max_episode_steps = env.spec.max_episode_steps
+        env = ContinuingTimeLimit(env, max_episode_steps=max_episode_steps)
+    # wrap env: observation...
+    # NOTE: wrapping order matters!
+    env = FrameSkip(env, skip=4)
+    env = ObtainPoVWrapper(env)
+    # convert hwc -> chw as Chainer requires.
+    env = MoveAxisWrapper(env, source=-1, destination=0)
+    env = SFF(env)
+    env = FrameStack(env, 4, channel_order='chw')
+    # wrap env: action...
+    env = SerialDiscreteActionWrapper(
+        env,
+        always_keys=['attack'], reverse_keys=['forward'], exclude_keys=['back', 'left', 'right', 'sneak', 'sprint'], exclude_noop=False)
 
-        return env
-
-def scaleFrames(frames):
-    for frame in frames:
-        frame = np.array(frame).astype(np.float32) / 255.0
-
-    return frames
+    return env
 
 
 def parse_agent(agent):
@@ -67,6 +53,7 @@ def parse_agent(agent):
             'DoubleDQN': chainerrl.agents.DoubleDQN,
             'PAL': chainerrl.agents.PAL,
             'CategoricalDoubleDQN': chainerrl.agents.CategoricalDoubleDQN}[agent]
+
 
 def get_agent(
         n_actions, arch, n_input_channels,
@@ -99,16 +86,15 @@ def get_agent(
     if prioritized:
         # Anneal beta from beta0 to 1 throughout training
         betasteps = steps / update_interval
-        rbuf = chainerrl.replay_buffer.PrioritizedReplayBuffer(
-            replay_capacity, alpha=0.5, beta0=0.4, betasteps=betasteps, num_steps=num_step_return)
+        rbuf = chainerrl.replay_buffer.PrioritizedReplayBuffer(replay_capacity, alpha=0.5, beta0=0.4, betasteps=betasteps, num_steps=num_step_return)
     else:
-        rbuf = chainerrl.replay_buffer.ReplayBuffer(
-            replay_capacity, num_step_return)
+        rbuf = chainerrl.replay_buffer.ReplayBuffer(replay_capacity, num_step_return)
 
     # build agent
     def phi(x):
         # observation -> NN input
         return np.asarray(x)
+
     Agent = parse_agent(agent_type)
     agent = Agent(
         q_func, opt, rbuf, gpu=gpu, gamma=gamma, explorer=explorer, replay_start_size=replay_start_size,
@@ -117,32 +103,32 @@ def get_agent(
 
     return agent
 
+
 def act(agent, obs):
-        with chainer.using_config('train', False), chainer.no_backprop_mode():
-            action_value, state, advantage = agent.model(
-                agent.batch_states([obs], agent.xp, agent.phi))
-            print("Q-Values: ", action_value.q_values.data[0])
-            print("Advantage: ", advantage)
-            print("State: ", state)
-            q = float(action_value.max.array)
-            action = cuda.to_cpu(action_value.greedy_actions.array)[0]
+    action_value = agent.model(
+        agent.batch_states([obs], agent.xp, agent.phi))
+    state = agent.model.state
+    print("Q-Values: ", action_value.q_values.data[0])
+    print("Advantage: ", agent.model.advantage)
+    print("State: ", state)
+    q = float(action_value.max.array)
+    action = cuda.to_cpu(action_value.greedy_actions.array)[0]
 
-        # Update stats
-        agent.average_q *= agent.average_q_decay
-        agent.average_q += (1 - agent.average_q_decay) * q
+    # Update stats
+    agent.average_q *= agent.average_q_decay
+    agent.average_q += (1 - agent.average_q_decay) * q
 
-        agent.logger.debug('t:%s q:%s action_value:%s', agent.t, q, action_value)
+    agent.logger.debug('t:%s q:%s action_value:%s', agent.t, q, action_value)
 
-        # if the state value is high enough, the agent is standing in front of a tree
-        if state >= 450:
-            print("CHOPPING TREE")
-            # execute action 1 to attack and toggle off 'forward'
-            action = 1
+    # if the state value is high enough, the agent is standing in front of a tree
+    if state >= 450:
+        print("CHOPPING TREE")
+        # execute action 1 to attack and toggle off 'forward'
+        action = 1
+    return action
 
-        return action
 
-
-def main():
+def main(gpu=-1, sleeping_amount=0.5):
     """
     This function will be called for training phase.
     """
@@ -160,34 +146,36 @@ def main():
         noisy_net_sigma=0.5, final_epsilon=0.01,
         final_exploration_frames=10 ** 6, explorer_sample_func=wrapped_env.action_space.sample,
         lr=0.0000625, adam_eps=0.00015, prioritized=True, steps=steps, update_interval=4,
-        replay_capacity=30000, num_step_return=10, agent_type='CategoricalDoubleDQN', gpu=-1, gamma=0.99, replay_start_size=5000,
+        replay_capacity=30000, num_step_return=10, agent_type='CategoricalDoubleDQN', gpu=gpu, gamma=0.99, replay_start_size=5000,
         target_update_interval=10000, clip_delta=True, batch_accumulator='mean'
     )
 
     agent.load("models/rainbow")
-
-    # av_q = 0
 
     for _ in range(MINERL_MAX_EVALUATION_EPISODES):
         obs = wrapped_env.reset()
         input("Press ENTER to continue")
         done = False
         netr = 0
+        step = 0
+        reward = 0
+        action = None
+
         while not done:
+            start = time.time()
             action = act(agent, obs)
             print("Action: ", action)
-            # print(agent.get_statistics())
-            # new_av_q = agent.get_statistics()[0][1]
-            # if new_av_q - av_q >= 0.002:
-            #     print("CHOP TREE")
-            # av_q = new_av_q
             obs, reward, done, info = wrapped_env.step(action)
-                
             netr += reward
             print("Net reward: ", netr)
-            time.sleep(0.5)
+            step += 1
+            sleep_time = sleeping_amount - (time.time() - start)
+            if sleep_time >= 0:
+                print(f"Sleeping {round(sleep_time)}s.")
+                time.sleep(sleep_time)
 
     wrapped_env.close()
+
 
 if __name__ == "__main__":
     main()
